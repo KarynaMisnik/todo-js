6cd0c95b88667a0db6bd971d47d3635ab572f221
KarynaMisnik <karinamisnik94@gmail.com>
2026-01-29 13:30:00 +0200

    semantic update

--- main.js
+++ main.js
@@ -118,3 +118,11 @@ addTodo.onclick = () => {
 // Initial render
 saveTodos();
 renderTodos();
+
+// Navbar toggle for mobile view
+const toggle = document.querySelector(".nav-toggle");
+const menu = document.querySelector(".nav-bar-container");
+
+toggle.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
